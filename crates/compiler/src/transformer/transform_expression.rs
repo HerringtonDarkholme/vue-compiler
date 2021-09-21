302efb5026176fc4bf758f441d0e1ff6d794047d
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2021-09-22 00:40:50 +0800

    [feat] add identifier track sketch

--- crates/compiler/src/transformer/transform_expression.rs
+++ crates/compiler/src/transformer/transform_expression.rs
@@ -15,6 +15,27 @@ pub struct ExpressionProcessor;
 impl<'a> CorePassExt<BaseInfo<'a>, Scope<'a>> for ExpressionProcessor {
     fn enter_root(&mut self, r: &mut BaseRoot<'a>, shared: &mut Scope<'a>) {}
     fn exit_root(&mut self, r: &mut BaseRoot<'a>, shared: &mut Scope<'a>) {}
-    fn enter_fn_param(&mut self, p: &mut Js<'a>, shared: &mut Scope<'a>) {}
-    fn exit_fn_param(&mut self, p: &mut Js<'a>, shared: &mut Scope<'a>) {}
+    fn enter_fn_param(&mut self, p: &mut Js<'a>, shared: &mut Scope<'a>) {
+        process_fn_param(p);
+        let a = match p {
+            Js::Simple(v, _) => *v,
+            Js::Compound(_) => todo!(),
+            _ => panic!("param should only be expression"),
+        };
+        *shared.identifiers.entry(a).or_default() += 1;
+    }
+    fn exit_fn_param(&mut self, p: &mut Js<'a>, shared: &mut Scope<'a>) {
+        let a = match p {
+            Js::Simple(v, _) => *v,
+            Js::Compound(_) => todo!(),
+            _ => panic!("param should only be expression"),
+        };
+        *shared.identifiers.entry(a).or_default() -= 1;
+    }
+}
+
+// parse expr as function params and breaks down binding pattern
+// e.g. [a, b, c] => identifiers a, b and c
+fn process_fn_param<'a>(p: &mut Js<'a>) {
+    todo!()
 }

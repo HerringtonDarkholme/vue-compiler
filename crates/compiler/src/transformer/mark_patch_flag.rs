547f1b8912604f7aad73051b2b3e6c782cd9f72d
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2021-09-23 18:52:48 +0800

    [feat] add inject key prop

--- crates/compiler/src/transformer/mark_patch_flag.rs
+++ crates/compiler/src/transformer/mark_patch_flag.rs
@@ -16,17 +16,26 @@ impl<'a> CorePassExt<BaseInfo<'a>, Scope<'a>> for PatchFlagMarker {
                     vn.is_block = true;
                 }
             }
-            // TODO: inject key prop
             let props = match &mut *branch.child {
                 IR::VNodeCall(v) => &mut v.props,
                 IR::RenderSlotCall(r) => &mut r.slot_props,
                 IR::For(f) => return f.key = Some(Js::Num(branch.info)),
                 _ => return,
             };
+            // already has key
+            if props.as_ref().map_or(false, find_key_on_js) {
+                return;
+            }
+            // inject default key
             let key = Js::StrLit(VStr::raw("key"));
             let val = Js::Num(branch.info);
             let key_prop = (key, val);
-            inject_prop(props, key_prop);
+
+            if let Some(ps) = props {
+                inject_prop(ps, key_prop);
+            } else {
+                *props = Some(Js::Props(vec![key_prop]));
+            }
         }
     }
     fn exit_for(&mut self, f: &mut BaseFor<'a>, shared: &mut Scope<'a>) {
@@ -45,19 +54,58 @@ impl<'a> CorePassExt<BaseInfo<'a>, Scope<'a>> for PatchFlagMarker {
 }
 
 fn find_key(t: &BaseIR) -> bool {
-    match t {
-        IR::VNodeCall(..) => todo!("read props"),
-        IR::RenderSlotCall(..) => todo!("read props"),
-        IR::AlterableSlot(..) => false,
+    let props = match t {
+        IR::VNodeCall(v) => &v.props,
+        IR::RenderSlotCall(r) => &r.slot_props,
+        IR::AlterableSlot(..) => return false,
         IR::VSlotUse(_) => {
             panic!("v-slot with v-for must be alterable slots")
         }
         IR::TextCall(_) | IR::For(_) | IR::If(_) | IR::CommentCall(_) => {
             panic!("v-for child must be vnode/renderSlot/slotfn")
         }
+    };
+    if let Some(prop) = props {
+        find_key_on_js(prop)
+    } else {
+        false
     }
 }
 
-fn inject_prop<'a>(props: &mut Option<Js<'a>>, key: Prop<'a>) {
-    todo!()
+fn find_key_on_js(e: &Js) -> bool {
+    match e {
+        Js::Call(RH::MergeProps, args) => args.iter().any(find_key_on_js),
+        Js::Props(ps) => ps.iter().any(|(k, _)| match k {
+            Js::StrLit(s) => s.raw == "key",
+            _ => false,
+        }),
+        _ => false,
+    }
+}
+
+// 1. mergeProps(...)
+// 2. toHandlers(...)
+fn inject_prop<'a>(props: &mut Js<'a>, key: Prop<'a>) {
+    debug_assert!(!find_key_on_js(props));
+    match props {
+        Js::Call(RH::MergeProps, args) => {
+            for arg in args.iter_mut() {
+                if let Js::Props(ps) = arg {
+                    ps.push(key);
+                    return;
+                }
+            }
+            args.push(Js::Props(vec![key]));
+        }
+        Js::Props(ps) => ps.push(key),
+        // should not inject props to any other expression
+        obj => {
+            return {
+                let mut temp = Js::Src("");
+                std::mem::swap(obj, &mut temp);
+                let p = Js::Props(vec![key]);
+                *obj = Js::Call(RH::MergeProps, vec![temp, p]);
+            }
+        }
+    }
 }

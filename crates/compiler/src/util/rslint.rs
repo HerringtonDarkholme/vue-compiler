aeaa3069e4d66c8b26e594bb24dde9b225a6629e
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2021-09-29 23:42:43 +0800

    [test] add more rslint test

--- crates/compiler/src/util/rslint.rs
+++ crates/compiler/src/util/rslint.rs
@@ -1,31 +1,66 @@
-use rslint_parser::{
-    self as rslint, ast::Expr, tokenize, LossyTreeSink, Parse, Parser, Syntax, TokenSource,
-};
+use rslint_parser::{ast::Expr, parse_expr, AstNode, SyntaxNodeExt};
+
+pub fn parse_js_expr(text: &str) -> Option<Expr> {
+    use std::ops::Range;
+    let parsed = parse_expr(text, 0);
+    if !parsed.errors().is_empty() {
+        return None;
+    }
+    // range should be equal after removing trailing trivia(comment/whitespace)
+    // otherwise the text is not a single expression
+    parsed.syntax().try_to().filter(|n: &Expr| {
+        let r: Range<usize> = Range::from(n.syntax().trimmed_range());
+        r.end - r.start == text.trim().len()
+    })
+}
 
 // copied from parse_expr
-fn parse_expr_lossy(text: &str) -> Parse<Expr> {
-    let file_id = 0;
-    let (tokens, mut errors) = tokenize(text, file_id);
-    let tok_source = TokenSource::new(text, &tokens);
-    let mut parser = Parser::new(tok_source, file_id, Syntax::default());
-    rslint::syntax::expr::expr(&mut parser);
-    let (events, p_diags) = parser.finish();
-    errors.extend(p_diags);
-    let mut tree_sink = LossyTreeSink::new(text, &tokens);
-    rslint::process(&mut tree_sink, events, errors);
-    let (green, parse_errors) = tree_sink.finish();
-    Parse::new(green, parse_errors)
+pub fn parse_fn_param(text: &str) -> Option<Expr> {
+    todo!()
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use rslint_parser::SyntaxNodeExt;
+    use crate::cast;
+    use rslint_parser::ast::{AstNode, BinOp, IfStmt};
 
     #[test]
-    fn print_ast() {
-        let a = parse_expr_lossy("a + b");
-        let b = a.syntax().to::<Expr>();
-        println!("{:?}", b);
+    #[should_panic]
+    fn test_panic_wrong_cast() {
+        let a = parse_expr("a + b", 0);
+        let b = a.syntax().to::<IfStmt>();
+    }
+    #[test]
+    fn test_no_panic() {
+        let a = parse_js_expr("a + b").unwrap();
+        let expr = cast!(a, Expr::BinExpr);
+        let a = expr.lhs().unwrap();
+        let b = expr.rhs().unwrap();
+        assert_eq!(a.syntax().text(), "a");
+        assert_eq!(expr.op().unwrap(), BinOp::Plus);
+        assert_eq!(b.syntax().text(), "b");
+    }
+
+    #[test]
+    fn test_syntax_range() {
+        let s = "    a +     b";
+        let a = parse_js_expr(s).unwrap();
+        let expr = cast!(a, Expr::BinExpr);
+        let a = expr.lhs().unwrap();
+        let b = expr.rhs().unwrap();
+        assert_eq!(&s[a.range()], "a");
+        assert_eq!(expr.op().unwrap(), BinOp::Plus);
+        assert_eq!(&s[b.range()], "b");
+    }
+    #[test]
+    fn test_invalid_expr() {
+        assert!(parse_js_expr("(a + b + c, d, e,f)").is_some());
+        assert!(parse_js_expr("a..b").is_none());
+        assert!(parse_js_expr("a // b").is_none());
+        assert!(parse_js_expr(" a + b ").is_some());
+        assert!(parse_js_expr("a **** b").is_none());
+        assert!(parse_js_expr("a; ddd;").is_none());
+        assert!(parse_js_expr("if (a) {b} else {c}").is_none());
     }
 }

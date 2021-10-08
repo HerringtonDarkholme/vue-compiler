883609a1f5d98d7e99d96f6c66e2719e727ad7ce
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2021-10-08 12:26:59 +0800

    [feat] add wasm bind

--- crates/wasm/src/lib.rs
+++ crates/wasm/src/lib.rs
@@ -1,14 +1,16 @@
 use wasm_bindgen::prelude::*;
+use compiler::compiler::{BaseCompiler, TemplateCompiler, get_base_passes};
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub fn base_compile(source: &str) -> String {
+    let sfc_info = Default::default();
+    let option = Default::default();
+    let passes = get_base_passes(&sfc_info, &option);
+    let mut ret = vec![];
+    let mut compiler = BaseCompiler::new(&mut ret, passes, option);
+    compiler.compile(source).unwrap();
+    String::from_utf8(ret).unwrap()
 }

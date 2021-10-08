6134ba4e2bae5ea2e17bea05d1a12cc3cdb8c220
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2021-10-08 10:55:36 +0800

    [feat] integrate wasm-bindgen

---
+++ crates/wasm/src/lib.rs
@@ -0,0 +1,14 @@
+use wasm_bindgen::prelude::*;
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}

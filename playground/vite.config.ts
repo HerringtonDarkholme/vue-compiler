2c44e136826cad04ffac1e069a06d418028d5435
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2021-10-08 16:57:44 +0800

    [fix] fix vite build config

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -3,5 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  base: '/vue-compiler/',
+  plugins: [vue()],
+  server: {
+    fs: {
+      // Allow serving files from one level up to the project root
+      allow: ['..']
+    }
+  }
 })

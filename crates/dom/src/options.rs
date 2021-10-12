f908dfb61dfc517abcf881cd698f483beed79c47
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2021-10-12 19:51:17 +0800

    [feat] implement tag options

--- crates/dom/src/options.rs
+++ crates/dom/src/options.rs
@@ -3,25 +3,45 @@ use compiler::{
     flags::RuntimeHelper, parser::Element, scanner::TextMode,
 };
 use crate::{converter::DOM_DIR_CONVERTERS, extension::dom_helper};
+use phf::{phf_set, Set};
 
-fn is_html_tag(s: &str) -> bool {
-    todo!()
-}
-
-fn is_svg_tag(s: &str) -> bool {
-    todo!()
-}
+const NATIVE_TAGS: Set<&str> = phf_set! {
+    // HTML_TAGS
+    "html","body","base","head","link","meta","style","title","address","article","aside","footer",
+    "header","h1","h2","h3","h4","h5","h6","nav","section","div","dd","dl","dt","figcaption", "figure",
+    "picture","hr","img","li","main","ol","p","pre","ul","a","b","abbr","bdi","bdo","br","cite","code",
+    "data","dfn","em","i","kbd","mark","q","rp","rt","ruby","s","samp","small","span","strong","sub","sup",
+    "time","u","var","wbr","area","audio","map","track","video","embed","object","param","source",
+    "canvas","script","noscript","del","ins","caption","col","colgroup","table","thead","tbody","td",
+    "th","tr","button","datalist","fieldset","form","input","label","legend","meter","optgroup",
+    "option","output","progress","select","textarea","details","dialog","menu",
+    "summary","template","blockquote","iframe","tfoot",
+    // SVG_TAGS
+    "svg","animate","animateMotion","animateTransform","circle","clipPath","color-profile",
+    "defs","desc","discard","ellipse","feBlend","feColorMatrix","feComponentTransfer",
+    "feComposite","feConvolveMatrix","feDiffuseLighting","feDisplacementMap",
+    "feDistanceLight","feDropShadow","feFlood","feFuncA","feFuncB","feFuncG","feFuncR",
+    "feGaussianBlur","feImage","feMerge","feMergeNode","feMorphology","feOffset",
+    "fePointLight","feSpecularLighting","feSpotLight","feTile","feTurbulence","filter",
+    "foreignObject","g","hatch","hatchpath","image","line","linearGradient","marker","mask",
+    "mesh","meshgradient","meshpatch","meshrow","metadata","mpath","path","pattern",
+    "polygon","polyline","radialGradient","rect","set","solidcolor","stop","switch","symbol",
+    "text","textPath","tspan","unknown","use","view", // "title"
+};
 
 fn is_native_tag(s: &str) -> bool {
-    is_html_tag(s) || is_svg_tag(s)
+    NATIVE_TAGS.contains(s)
 }
-
 fn is_pre_tag(s: &str) -> bool {
     s.eq_ignore_ascii_case("pre")
 }
 
+const VOID_TAGS: &[&str] = &[
+    "area", "base", "br", "col", "embed", "hr", "img", "input", "link", "meta", "param", "source",
+    "track", "wbr",
+];
 fn is_void_tag(s: &str) -> bool {
-    todo!()
+    VOID_TAGS.contains(&s)
 }
 
 fn get_builtin_component(s: &str) -> Option<RuntimeHelper> {

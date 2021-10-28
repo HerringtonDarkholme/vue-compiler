30dd2ea366071c70f288e0b1af26acc7ab891a8f
Herrington Darkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2021-10-28 11:06:09 +0800

    [feat] add rewrite default

--- crates/sfc/src/rewrite_default.rs
+++ crates/sfc/src/rewrite_default.rs
@@ -1,1 +1,29 @@
-pub fn rewrite_default() {}
+pub fn rewrite_default(input: String, _as_var: &'static str) -> String {
+    if !has_any_default_export(&input) {
+        return input;
+    }
+    todo!()
+}
+
+/*
+const defaultExportRE = /((?:^|\n|;)\s*)export(\s*)default/
+const namedDefaultExportRE = /((?:^|\n|;)\s*)export(.+)as(\s*)default/s
+const exportDefaultClassRE =
+  /((?:^|\n|;)\s*)export\s+default\s+class\s+([\w$]+)/
+ * */
+
+fn has_any_default_export(input: &str) -> bool {
+    has_export_default(input) || has_named_default_export(input)
+}
+fn has_export_default(input: &str) -> bool {
+    let _idx = input.find("export ");
+    todo!()
+}
+fn has_named_default_export(_input: &str) -> bool {
+    todo!()
+}
+
+fn is_start_of_statement(input: &str, pos: usize) -> bool {
+    let input = input[..pos].trim_end_matches(char::is_whitespace);
+    input.chars().last().unwrap_or(';') == ';'
+}

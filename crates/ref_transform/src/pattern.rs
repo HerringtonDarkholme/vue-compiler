0eb8b727e3f293b102014810448bf3a2b8caa61a
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2021-12-18 01:46:29 -0500

    [feat] implement first version of tree matching

--- crates/ref_transform/src/pattern.rs
+++ crates/ref_transform/src/pattern.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use super::Node;
+use tree_sitter::{Node as TNode};
 
 pub struct MetaVariable {
     meta_var_regex: Option<String>,
@@ -17,7 +18,7 @@ impl Pattern {
             meta_variables: extract_meta_vars(src),
         }
     }
-    pub fn match_node<'cand, 's>(&'s self, node: &'cand Node) -> Option<&'cand Node> {
+    pub fn match_node<'tree>(&'tree self, node: &'tree Node) -> Option<TNode<'tree>> {
         match_node(&self.pattern_node, node)
     }
     pub fn gen_replaced(&self, vars: HashMap<String, String>) -> String {
@@ -25,8 +26,31 @@ impl Pattern {
     }
 }
 
-fn match_node<'cand, 's>(_goal: &'s Node, _candidate: &'cand Node) -> Option<&'cand Node> {
-    todo!()
+fn match_node<'tree>(
+    goal: &'tree Node,
+    candidate: &'tree Node,
+) -> Option<tree_sitter::Node<'tree>> {
+    let goal = goal.inner.root_node();
+    let candidate = candidate.inner.root_node();
+    match_impl(&goal, candidate)
+}
+fn match_impl<'tree>(goal: &TNode<'tree>, candidate: TNode<'tree>) -> Option<TNode<'tree>> {
+    let mut cursor = candidate.walk();
+    if goal.kind_id() == candidate.kind_id() {
+        let match_children = candidate
+            .children(&mut cursor)
+            .enumerate()
+            .all(|(i, n)| match_impl(&goal.child(i).unwrap(), n).is_some());
+        if match_children {
+            Some(candidate)
+        } else {
+            None
+        }
+    } else {
+        candidate
+            .children(&mut cursor)
+            .find_map(|n| match_impl(goal, n))
+    }
 }
 
 fn extract_meta_vars(_src: &str) -> HashMap<String, MetaVariable> {

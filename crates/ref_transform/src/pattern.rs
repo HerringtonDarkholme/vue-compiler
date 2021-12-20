6cf090ff9a073d376d263f45b66ed5c36f4f898b
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2021-12-19 23:57:55 -0500

    [feat] add match meta variable env

--- crates/ref_transform/src/pattern.rs
+++ crates/ref_transform/src/pattern.rs
@@ -2,6 +2,8 @@ use std::collections::HashMap;
 use crate::Node;
 use tree_sitter::{Node as TNode};
 
+pub type Env<'tree> = HashMap<String, TNode<'tree>>;
+
 pub struct MetaVariable {
     meta_var_regex: Option<String>,
 }
@@ -18,10 +20,10 @@ impl Pattern {
             meta_variables: extract_meta_vars(src),
         }
     }
-    pub fn match_node<'tree>(&'tree self, node: &'tree Node) -> Option<TNode<'tree>> {
+    pub fn match_node<'tree>(&'tree self, node: &'tree Node) -> Option<(TNode<'tree>, Env<'tree>)> {
         match_node(&self.pattern_node, node)
     }
-    pub fn gen_replaced(&self, vars: HashMap<String, String>) -> String {
+    pub fn gen_replaced(&self, _vars: Env) -> String {
         todo!()
     }
 }
@@ -29,7 +31,8 @@ impl Pattern {
 fn match_node<'tree>(
     goal: &'tree Node,
     candidate: &'tree Node,
-) -> Option<tree_sitter::Node<'tree>> {
+) -> Option<(TNode<'tree>, Env<'tree>)> {
+    let mut env = HashMap::new();
     let source = &goal.source;
     let goal = goal.inner.root_node();
     if goal.child_count() != 1 {
@@ -40,21 +43,24 @@ fn match_node<'tree>(
     if candidate.next_sibling().is_some() {
         todo!("multi candidate roots are not supported yet.")
     }
-    match_impl(&goal, candidate, source)
+    let node = match_impl(&goal, candidate, source, &mut env)?;
+    Some((node, env))
 }
 fn match_impl<'tree>(
     goal: &TNode<'tree>,
     candidate: TNode<'tree>,
     source: &str,
+    env: &mut Env<'tree>,
 ) -> Option<TNode<'tree>> {
     let is_leaf = goal.child_count() == 0;
-    if is_leaf
-        && is_wildcard_pattern(
-            goal.utf8_text(source.as_bytes())
-                .expect("invalid source pattern encoding"),
-        )
-    {
-        return Some(candidate);
+    if is_leaf {
+        let key = goal
+            .utf8_text(source.as_bytes())
+            .expect("invalid source pattern encoding");
+        if is_wildcard_pattern(key) {
+            env.insert(key.to_owned(), candidate);
+            return Some(candidate);
+        }
     }
     if goal.kind_id() == candidate.kind_id() {
         if is_leaf {
@@ -67,7 +73,7 @@ fn match_impl<'tree>(
         if !candidate_cursor.goto_first_child() {
             return None;
         }
-        while match_impl(&goal_cursor.node(), candidate_cursor.node(), source).is_some() {
+        while match_impl(&goal_cursor.node(), candidate_cursor.node(), source, env).is_some() {
             if !goal_cursor.goto_next_sibling() {
                 // all goal found, return
                 return Some(candidate);
@@ -80,7 +86,7 @@ fn match_impl<'tree>(
     } else {
         let mut cursor = candidate.walk();
         let mut children = candidate.children(&mut cursor);
-        children.find_map(|sub_cand| match_impl(goal, sub_cand, source))
+        children.find_map(|sub_cand| match_impl(goal, sub_cand, source, env))
     }
 }
 
@@ -137,6 +143,15 @@ mod test {
         test_match("const a = $VALUE", "const a = 123");
         test_match("const $VARIABLE = $VALUE", "const a = 123");
         test_match("const $VARIABLE = $VALUE", "const a = 123");
+    }
+
+    #[test]
+    fn test_meta_variable_env() {
+        let cand_str = "const a = 123";
+        let goal = Pattern::new("const a = $VALUE");
+        let cand = Pattern::new(cand_str);
+        let (_, env) = match_node(&goal.pattern_node, &cand.pattern_node).unwrap();
+        assert_eq!(env["$VALUE"].utf8_text(cand_str.as_bytes()).unwrap(), "123");
     }
 
     #[test]

f521057852d096b9f96b7894d039bbb578ec5770
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2022-01-03 01:37:06 -0500

    [feat] refine matcher nested cases

--- crates/ref_transform/src/matcher.rs
+++ crates/ref_transform/src/matcher.rs
@@ -16,6 +16,47 @@ pub fn match_single_kind<'tree>(
     children.find_map(|sub_cand| match_single_kind(goal_kind, sub_cand, env))
 }
 
+pub fn match_node_exact<'tree>(
+    goal: &TNode<'tree>,
+    candidate: TNode<'tree>,
+    source: &str,
+    env: &mut Env<'tree>,
+) -> Option<TNode<'tree>> {
+    let is_leaf = goal.child_count() == 0;
+    if is_leaf {
+        let key = goal
+            .utf8_text(source.as_bytes())
+            .expect("invalid source pattern encoding");
+        if is_meta_var(key) {
+            env.insert(key.to_owned(), candidate);
+            return Some(candidate);
+        }
+    }
+    if goal.kind_id() != candidate.kind_id() {
+        return None;
+    }
+    if is_leaf {
+        return Some(candidate);
+    }
+    let mut goal_cursor = goal.walk();
+    let moved = goal_cursor.goto_first_child();
+    debug_assert!(moved);
+    let mut candidate_cursor = candidate.walk();
+    if !candidate_cursor.goto_first_child() {
+        return None;
+    }
+    while match_node_exact(&goal_cursor.node(), candidate_cursor.node(), source, env).is_some() {
+        if !goal_cursor.goto_next_sibling() {
+            // all goal found, return
+            return Some(candidate);
+        }
+        if !candidate_cursor.goto_next_sibling() {
+            return None;
+        }
+    }
+    None
+}
+
 pub fn match_node_impl<'tree>(
     goal: &TNode<'tree>,
     candidate: TNode<'tree>,
@@ -43,7 +84,8 @@ pub fn match_node_impl<'tree>(
         if !candidate_cursor.goto_first_child() {
             return None;
         }
-        while match_node_impl(&goal_cursor.node(), candidate_cursor.node(), source, env).is_some() {
+        while match_node_exact(&goal_cursor.node(), candidate_cursor.node(), source, env).is_some()
+        {
             if !goal_cursor.goto_next_sibling() {
                 // all goal found, return
                 return Some(candidate);
@@ -94,6 +136,9 @@ mod test {
     }
     #[test]
     fn test_extact_match_should_not_match_inner() {
-        // test_non_match("function foo() { let a = 123; }", "function foo() { function bar() {let a = 123; }}");
+        test_non_match(
+            "function foo() { let a = 123; }",
+            "function foo() { function bar() {let a = 123; }}",
+        );
     }
 }

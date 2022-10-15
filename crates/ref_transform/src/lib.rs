dce892041064609ffe9a81e67e7b651b129b7e9e
HerrintonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2022-10-15 00:15:06 -0400

    [fix] fix errors

--- crates/ref_transform/src/lib.rs
+++ crates/ref_transform/src/lib.rs
@@ -1,4 +1,16 @@
-use ast_grep_core;
-use ast_grep_config;
+use ast_grep_core::{
+    AstGrep,
+    language::{Language, TSLanguage},
+};
 
-pub fn test() {}
+#[derive(Clone)]
+pub struct Ts;
+impl Language for Ts {
+    fn get_ts_language(&self) -> TSLanguage {
+        tree_sitter_typescript::language_tsx().into()
+    }
+}
+
+pub fn test() {
+    AstGrep::new("console.log(123)", Ts);
+}

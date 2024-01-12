a94b7c83a90f9257fc45d418512aad420139cb4f
HerrintonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2024-01-11 17:15:12 -0800

    fix: use dom setup for playground

--- crates/wasm/src/lib.rs
+++ crates/wasm/src/lib.rs
@@ -2,7 +2,11 @@
 // https://github.com/rustwasm/wasm-bindgen/issues/2774
 
 use wasm_bindgen::prelude::*;
-use compiler::compiler::{BaseCompiler, TemplateCompiler, get_base_passes};
+use compiler::compiler::{BaseCompiler, TemplateCompiler};
+use compiler::error::NoopErrorHandler;
+use dom::{get_dom_pass, compile_option};
+
+use std::rc::Rc;
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
@@ -10,9 +14,9 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[wasm_bindgen(js_name = baseCompile)]
 pub fn base_compile(source: &str) -> String {
     let sfc_info = Default::default();
-    let option = Default::default();
+    let option = compile_option(Rc::new(NoopErrorHandler));
     let dest = Vec::new;
-    let compiler = BaseCompiler::new(dest, get_base_passes, option);
+    let compiler = BaseCompiler::new(dest, get_dom_pass, option);
     let ret = compiler.compile(source, &sfc_info).unwrap();
     String::from_utf8(ret).unwrap()
 }
